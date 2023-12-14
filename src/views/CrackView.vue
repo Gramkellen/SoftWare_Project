9d31ec3520429981bfb7773c29cc3371aecd0590
3232321 <134861398+3232321@users.noreply.github.com>
2023-12-14 11:03:18 +0800

    更新内爆，home

--- src/views/CrackView.vue
+++ src/views/CrackView.vue
@@ -1,13 +1,52 @@
+
 <template>
     <div>
-        <p>这里是玻璃幕墙裂爆裂检测页面</p>
+        <p>玻璃幕墙裂爆裂检测页面</p>
+        <!-- 尝试图片上传组件 -->
+        <ImgUpload 
+        @confirmUpload="confirmUpload"
+        @onCancel="onCancel"
+        class="img-uploader"
+        >
+        </ImgUpload>
+
+        <div class="result-list-container"
+        v-if="showResult">
+            <p>这里将展示后端返回的结果</p>
+        </div>
     </div>
 </template>
 
 <script setup>
+import ImgUpload  from '@/components/ImgUpload.vue'
+import { ref } from 'vue'
+
+const showResult = ref(false); // 决定是否展示结果框
+
+// 确认上传
+const confirmUpload = (file) => {
+    // 拿到的file是ImgUpload传递过来的用户决定上传的图片
+    console.log("parent has recieved params passed by child");
+    console.log(file);
+    // 接着就是把图片上传到后端
+    // 然后在界面上显示信息
+    showResult.value = true;
+
+}
+
+// 更换了想要检测的图片
+const onCancel = () => {
+    console.log("you have cancel the upload");
+    showResult.value = false; // 将刚才拿到的结果清空
+}
+
 
 </script>
 
 <style lang="scss" scoped>
-
+.result-list-container {
+    width:70%;
+    height:70%;
+    border:black 1em;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="home">
-    <div class="title-container">
-      <p class="title">玻璃幕墙信息系统</p>
+  <div>
+    <div class="header">
+      <transition name="el-zoom-in-center">
+        <img v-show="show" @load="handleImageLoad" src="@/assets/高楼1.jpg" alt="Your Image" class="header-image" />
+      </transition>
+      <div class="title-container">
+        <p class="title">玻璃幕墙信息系统</p>
+      </div>
     </div>
     <div class="brief-introduction-container">
       <p class="brief-introduction">
@@ -12,20 +17,50 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
 
+const show = ref(false);
+
+const handleImageLoad = () => {
+  show.value = true;
+};
+
+onMounted(() => {
+  setTimeout(() => {
+    show.value = true;
+  }, 200); // 延时500毫秒触发过渡效果
+});
 </script>
 
 <style lang="scss" scoped>
+.header {
+  position: relative;
+}
+
+.header-image {
+  width: 100%;
+  height: auto;
+  display: block;
+}
+
 .title-container {
-  justify-content: center;
-  
-  .title{
-    font-size: 40;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+
+  .title {
+    font-size: 40px;
     font-weight: bolder;
+    color: white; // 设置文字颜色为白色，以适应可能的背景
+    letter-spacing: 5px; // 调整这个值以实现你想要的字间距
   }
 }
 
 .brief-introduction-container {
+  display: flex;
   justify-content: center;
+  text-align: center;
 }
 </style>
