2e60fc58a69d7169b6215839293946c17886b44f
Redwood-Jia <786783037@qq.com>
2023-12-16 11:43:20 +0800

    测试上传接口/成功

--- src/views/EvennessView.vue
+++ src/views/EvennessView.vue
@@ -18,18 +18,29 @@
 
 <script setup>
 import ImgUpload  from '@/components/ImgUpload.vue'
+import axios from 'axios'
 import { ref } from 'vue'
 
 const showResult = ref(false); // 决定是否展示结果框
+const imgUploadAPI = "http://pjh754.cn:8000/";
 
 // 确认上传
-const confirmUpload = (file) => {
+const confirmUpload = async (file) => {
     // 拿到的file是ImgUpload传递过来的用户决定上传的图片
     console.log("parent has recieved params passed by child");
-    console.log(file);
     // 接着就是把图片上传到后端
     // 然后在界面上显示信息
+    const formData = new FormData();
+    formData.append('photo', file.fileList[0].raw);
+
     showResult.value = true;
+    try {
+        const res = await axios.post(imgUploadAPI,formData);
+        console.log(res);
+    } catch(e) {
+        console.log("接口调用失败！");
+        console.log(e);
+    }
 
 }
 
@@ -38,7 +49,6 @@ const onCancel = () => {
     console.log("you have cancel the upload");
     showResult.value = false; // 将刚才拿到的结果清空
 }
-
 
 </script>
 
